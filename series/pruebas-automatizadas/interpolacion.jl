affb716f6fbe2cc64ae305328571675a71a557bd
augusto kielbowicz <augusto.kiel@gmail.com>
2024-08-27 21:27:10 -0300

    [serie/pruebas-automatizadas] parte 2

---
+++ series/pruebas-automatizadas/interpolacion.jl
@@ -0,0 +1,24 @@
+function interpolacion(puntos)
+    d = Dict(puntos)
+    f(x) = haskey(d,x) ? d[x] : 0
+    f
+end
+
+segundo(par) = par[2]
+en_x(p) = map(first, p)
+en_y(p) = map(segundo, p)
+
+
+function interpolacion_constante(puntos)
+    xs = en_x(puntos)
+    ys = en_y(puntos)
+    function indice_de(i) 
+         indice = findfirst(i .== xs)
+         if !isnothing(indice)
+            return indice
+         end
+         findfirst(abs.(diff(i .> xs)) .> 0)
+    end
+    f(x) = ys[indice_de(x)] 
+    f
+end
