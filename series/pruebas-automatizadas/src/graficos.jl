d1710f9f788c4bc6db38959f3a970d6bc66860c7
augusto kielbowicz <augusto.kiel@gmail.com>
2024-09-05 21:01:42 -0300

    agregar mejoras en el formato del grafico

--- series/pruebas-automatizadas/src/graficos.jl
+++ series/pruebas-automatizadas/src/graficos.jl
@@ -7,6 +7,7 @@ function graficar(f, puntos)
     # x2 = -1.0:0.1:2.0
     xmin, xmax = extrema(x)
     x2 = range(xmin, xmax, 100)
-    scatter(x2, f.(x2), label="muestra")
-    scatter!(x, f.(x), label="Puntos de interpolación")
+    scatter(x2, f.(x2), ms=2, label="muestra")
+    scatter!(x, f.(x), ms=5, ma=0.5, mc=:black, label="Puntos de interpolación")
+    plot!(legend=:outerbottom, legendcolumns=2)
 end
