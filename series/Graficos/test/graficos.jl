103f891d65115dca7d905fa33c0f3027b7f533f9
augusto kielbowicz <augusto.kiel@gmail.com>
2024-09-06 19:56:15 -0300

    agregar primera implementacion del codigo para graficar con un svg

--- series/Graficos/test/graficos.jl
+++ series/Graficos/test/graficos.jl
@@ -1,2 +1,10 @@
 using Graficos
-@test Graficos.greet() == "chao"
+
+@testset "Test1" begin
+    data = [(10,10),(200,200),(400,400)]
+    svg = Graficos.template(data, "blue");
+
+    esperado = read(open(joinpath(@__DIR__,"datos_pruebas","scatter.svg")),String)
+
+    @test svg == esperado
+end
