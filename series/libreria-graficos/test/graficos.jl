176e95ea93a68f789a7468a05f94c26368914cea
akielbowicz <augusto.kiel@gmail.com>
2025-01-07 18:41:32 -0300

    [serie/l-g] p2: Configurar tests locales y en CI/CD

--- series/libreria-graficos/test/graficos.jl
+++ series/libreria-graficos/test/graficos.jl
@@ -1,10 +1,14 @@
 using Graficos
+using TestItem
 
-@testset "Test1" begin
-    data = [(10,10),(200,200),(400,400)]
-    svg = Graficos.template(data, "blue");
 
-    esperado = read(open(joinpath(@__DIR__,"datos_pruebas","scatter.svg")),String)
+@testitem "Test interfaz de Graficos" begin
+    @testset "Test1" begin
+        data = [(10,10),(200,200),(400,400)]
+        svg = Graficos.template(data, "blue");
 
-    @test svg == esperado
+        esperado = read(open(joinpath(@__DIR__,"datos_pruebas","scatter.svg")),String)
+
+        @test svg == esperado
+    end
 end
