515aedc5486a7978ca90c8fc8f74b905b3e4e19b
akielbowicz <augusto.kiel@gmail.com>
2025-01-07 19:34:29 -0300

    agregar escaleo de los puntos al dominio de la imagen

--- series/libreria-graficos/main.jl
+++ series/libreria-graficos/main.jl
@@ -2,4 +2,12 @@
 
 using Graficos
 
-Graficos.template([(10,10), (20,20), (30,30)]) |> Graficos.mostrar
+
+data = [(10,10), (20,20), (30,30)]
+Graficos.template(data) |> Graficos.mostrar
+
+
+
+
+
+d2 = escalear(data, 400, 400)
