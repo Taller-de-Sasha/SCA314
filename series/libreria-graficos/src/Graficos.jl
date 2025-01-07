e9931c1d311efd3914f4d791a3f325aa21b84b49
akielbowicz <augusto.kiel@gmail.com>
2025-01-07 19:34:29 -0300

    agregar escaleo de los puntos al dominio de la imagen

--- series/libreria-graficos/src/Graficos.jl
+++ series/libreria-graficos/src/Graficos.jl
@@ -4,12 +4,29 @@ width = 400
 height = 400
 
 como_string(x,y) = "$x,$y"
-puntos(data) = join(map( p ->como_string(p[1], height-p[2]),data)," ")
+
+
+# Tomar el minimo y maximo de cada eje y escalar los datos al 90% del tamaño del eje
+function escalear_(data, width, height)
+  min_x, max_x = extrema(map(x -> x[1], data))
+  min_y, max_y = extrema(map(x -> x[2], data))
+   scale = 1.0
+   shift = 10
+  x_scale = scale * width / (max_x - min_x)
+  y_scale = scale * height / (max_y - min_y)
+  return [(x_scale * (x - min_x )+shift, y_scale * (y - min_y)+shift) for (x, y) in data]
+end
+
+function escalear(data)
+    escalear_(data, width-20, height-20)
+end
+
+puntos(data) = join(map( p ->como_string(p[1], height-p[2]), escalear(data))," ")
 
 function template(data, color="red") 
    points = puntos(data)
 """
-<svg height="$height" width="$width" xmlns="http://www.w3.org/2000/svg" style="background-color: white; border: 1px solid black"> 
+<svg height="$height" width="$width" xmlns="http://www.w3.org/2000/svg" style="background-color: white; border: 10px solid black"> 
   <defs>
     <!-- Dot marker definition -->
     <marker
@@ -22,6 +39,8 @@ function template(data, color="red")
       <circle cx="5" cy="5" r="5" fill="$color" />
     </marker>
   </defs>
+
+  <rect x="10" y="10" width="$(width)" height="$(height)" fill="none" stroke="black" stroke-width="1" />
   
     <polyline
     points="$points"
