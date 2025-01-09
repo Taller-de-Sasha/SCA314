9f794d58741b2198ff690f2c82cba32cae7a9d7a
akielbowicz <augusto.kiel@gmail.com>
2025-01-09 19:48:26 -0300

    completar configuraciones de la documentación

--- series/libreria-graficos/docs/make.jl
+++ series/libreria-graficos/docs/make.jl
@@ -2,3 +2,7 @@ push!(LOAD_PATH,"../src/")
 using Documenter, Graficos
 
 makedocs(sitename="Graficos", repo="https://github.com/Taller-de-Sasha/sca314.git")
+
+deploydocs(
+    repo="https://github.com/Taller-de-Sasha/sca314.git"
+)
