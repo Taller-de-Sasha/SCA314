d91382c30912be25bf35ca5cc82c5d2b6b78ba48
akielbowicz <augusto.kiel@gmail.com>
2025-04-24 20:45:57 -0300

    update docs/make.jl

--- series/libreria-graficos/docs/make.jl
+++ series/libreria-graficos/docs/make.jl
@@ -1,9 +1,11 @@
 push!(LOAD_PATH,"../src/")
 using Documenter, Graficos
 
-makedocs(sitename="Graficos", repo="https://github.com/Taller-de-Sasha/SCA314.git")
+repo="https://github.com/Taller-de-Sasha/SCA314.git"
+makedocs(;sitename="Graficos",repo)
 
-deploydocs(
-    repo="https://github.com/Taller-de-Sasha/SCA314.git",
-    devbranch="serie/l-g-p3",
+deploydocs(;
+    repo,
+    # devbranch="serie/l-g-p3",
+    dirname="documenter",
 )
